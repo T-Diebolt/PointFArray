12a3800753c2aa3818b3e169aa1ff5cf0872e832
Tony Theodoropoulos <theodot@outlook.com>
2022-02-16 09:31:47 -0500

    Add project files.

---
+++ PointFArray/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace PointFArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // drawButton
+            // 
+            this.drawButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drawButton.Location = new System.Drawing.Point(361, 355);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 41);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "DRAW";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 408);
+            this.Controls.Add(this.drawButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawButton;
+    }
+}
+
---
+++ PointFArray/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointFArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            Point point1 = new Point();
+
+            point1.X = 50;
+            point1.Y = 90;
+
+            Point point2 = new Point(150, 100);
+            Point point3 = new Point(100, 250);
+
+            Graphics g = this.CreateGraphics();
+            // should also have SolidBrush or Pen
+            Pen redPen = new Pen(Color.Red);
+
+            //g.DrawLine(redPen, point1, point2);
+            //g.DrawLine(redPen, point2, point3);
+            //g.DrawLine(redPen, point3, point1);
+
+            Point[] quad = new Point[4];
+            quad[0] = point1;
+            quad[1] = point2;
+            quad[2] = point3;
+            quad[3] = new Point(300, 53);
+
+            g.DrawPolygon(redPen, quad);
+            g.FillPolygon(Brushes.Blue, quad);
+
+        }
+    }
+}
